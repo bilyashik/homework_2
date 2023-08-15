24871dc26fab18284119acd32173b0100e24fc7e
bilyashik <138653003+bilyashik@users.noreply.github.com>
2023-08-15 11:01:49 +0600

    homework2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [color1, setColor1] = useState('red');
+  const [color2, setColor2] = useState('blue');
+
+  const swapColors = () => {
+    setColor1(color2);
+    setColor2(color1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        className="square"
+        style={{ backgroundColor: color1 }}
+        onClick={swapColors}
+      ></div>
+      <div
+        className="square"
+        style={{ backgroundColor: color2 }}
+        onClick={swapColors}
+      ></div>
     </div>
   );
 }
